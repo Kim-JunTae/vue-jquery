a637824d50c8113265fbe94fbdda7eb0887ca281
Jun <rardin@naver.com>
2020-09-24 01:06:51 +0900

    0924_vue-jquery

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    App 컴포넌트
+    <DatePicker></DatePicker>
+  </div>
+</template>
+
+<script>
+import DatePicker from './DatePicker.vue'
+
+export default {
+  // created(){
+  //   console.log($.fn.jquery);
+  //   console.log($.ui.version);
+  // }
+  // mounted(){
+  //   $('#calendar').datepicker();
+  // }
+  components:{
+    DatePicker
+  }
+}
+</script>
